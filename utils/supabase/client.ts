ce261d20afb26ab140aa303b6ccc2743bf049a96
Ben Luis <benluis@unc.edu>
2025-03-16 22:13:11 -0400

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,6 +2,6 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    'https://ftpyopsikzrqqcugoize.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0cHlvcHNpa3pycXFjdWdvaXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMTc2MjgsImV4cCI6MjA1NjU5MzYyOH0.MgoZk-6dzXvaJ9oKcuh8TFQlrh3xRdCoqahrmncdmmc\n',
   );
